cd143edbe5fe6fd3feaca1b436322a630bf63960
LasanthiLiyanage <75748796+LasanthiLiyanage@users.noreply.github.com>
2021-01-12 15:31:15 +0530

    Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,7 +32,8 @@ namespace API
             {
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
-            services.AddControllers();            
+            services.AddControllers(); 
+            services.AddCors();           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +43,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
